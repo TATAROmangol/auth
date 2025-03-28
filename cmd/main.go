5bd87807dc21d366b72ac88643548aed940d8a17
TATAROmangol <deadpoolmail.ru@gmail.com>
2025-03-29 02:38:21 +0500

    add logger

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"auth/internal/config"
+	"auth/pkg/logger"
 	"auth/pkg/postgres"
 	"context"
 )
@@ -10,7 +11,12 @@ func main() {
 	cfg := config.MustLoad()
 
 	ctx := context.Background()
+	ctx = logger.ImportInContext(ctx)
 
 	db, err := postgres.NewConnect(cfg.PG)
+	if err != nil{
+		logger.GetFromCtx(ctx).ErrorContext(ctx, "failed to connect postgres", "err", err)
+	}
 
+	
 }
